afe0cfde31aad26bea95fd3a4b1368c8439a1d7b
swust-xiaoj <swustxiaojie@163.com>
2017-10-25 13:25:40 +0800

    re init proj

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello></hello>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello';
-
 export default {
-  name: 'app',
-  components: {
-    Hello,
-  },
-};
+  name: 'app'
+}
 </script>
 
 <style>

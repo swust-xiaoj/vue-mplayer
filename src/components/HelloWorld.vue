32f3d1285f820eb69dab38116c749ab7fcd8ca29
swust-xiaoj <swustxiaojie@163.com>
2017-10-25 19:07:09 +0800

    add mint-ui

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,53 +1,46 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <mt-swipe :auto="2000" class='swiper'>
+      <mt-swipe-item class="slide1">1</mt-swipe-item>
+      <mt-swipe-item class="slide2">2</mt-swipe-item>
+      <mt-swipe-item class="slide3">3</mt-swipe-item>
+    </mt-swipe>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    name: 'HelloWorld',
+    data() {
+        return {
+            msg: 'Welcome to Your Vue.js App'
+        };
     }
-  }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.swiper {
+    height: 200px;
+    font-size: 20px;
 }
-
-ul {
-  list-style-type: none;
-  padding: 0;
+.slide1 {
+    background-color: #0089dc;
+    color: #fff;
+    line-height: 200px;
+    height: 200px;
 }
-
-li {
-  display: inline-block;
-  margin: 0 10px;
+.slide2 {
+    background-color: #ffd705;
+    color: #fff;
+    line-height: 200px;
+    height: 200px;
 }
-
-a {
-  color: #42b983;
+.slide3 {
+    background-color: #ff2d4b;
+    color: #fff;
+    line-height: 200px;
+    height: 200px;
 }
 </style>
